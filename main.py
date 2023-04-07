4753ea3e078051b81913f6b996065ab9db1b5f5d
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-07 16:13:09 +0100

    Check users guess to actual answer

--- main.py
+++ main.py
@@ -8,6 +8,17 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 from random import randint
+
+
+# Function to check user's guess against actual answer
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too high.")
+    elif guess < answer:
+        print("Too low.")
+    else:
+        print("You got it! The answer was {answer}.")
+
 # Choosing a random number between 1 & 100
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
@@ -16,8 +27,8 @@ answer = randint(1, 100)
 # Make function to set difficulty
 
 # Let user guess a number
+guess = int(input("Make a guess: "))
 
-# Function to check user's guess against actual answer
 
 # Track the number of turns remaining & reduce by 1 is they get it wrong
 
