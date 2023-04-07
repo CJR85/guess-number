a306b3a942ea554f43ee309b9898fd99d16c67d8
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-07 17:13:02 +0100

    Track number of guesses remaining, reduce by 1

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ HARD_LEVEL_TURNS = 5
 
 
 # Function to check user's guess against actual answer
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
+    """ Checks answer against the user's answer. Returns the number of turns remaining."""
     if guess > answer:
         print("Too high.")
+        return turns - 1
     elif guess < answer:
         print("Too low.")
+        return turns - 1
     else:
         print(f"You got it! The answer is {answer}.")
 
@@ -39,16 +42,21 @@ def game():
   print(f"The correct answer is {answer}.")
 
   turns = set_difficulty()
-  print(f"You have {turns} attempts remaining to guess the number.")
 
   # Repeat the guessing functionality if they get it wrong
   guess = 0
   while guess != answer:
+    print(f"You have {turns} attempts remaining to guess the number.")
+    
     # Let user guess a number
     guess = int(input("Make a guess: "))
-    check_answer(guess, answer)
+    # Track the number of turns remaining & reduce by 1 is they get it wrong
+    turns = check_answer(guess, answer, turns)
+    if turns == 0:
+        print("You've run out of guesses, you lose.")
+        return
+    elif guess != answer:
+        print("Guess again.")
 
-
-# Track the number of turns remaining & reduce by 1 is they get it wrong
 
 game()
