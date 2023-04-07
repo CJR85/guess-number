9bdad2ca08c6c8ef7adad229770952669a08b31f
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-07 16:08:42 +0100

    Print intro to game

--- main.py
+++ main.py
@@ -7,3 +7,18 @@
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+from random import randint
+# Choosing a random number between 1 & 100
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1, 100)
+
+# Make function to set difficulty
+
+# Let user guess a number
+
+# Function to check user's guess against actual answer
+
+# Track the number of turns remaining & reduce by 1 is they get it wrong
+
+# Repeat the guessing if they get it wrong
