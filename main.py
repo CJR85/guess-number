b59d115b8ef25a32a006ea8f08f4f7870864aebc
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-07 16:38:52 +0100

    Repeat the guessing functionality

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def check_answer(guess, answer):
     elif guess < answer:
         print("Too low.")
     else:
-        print("You got it! The answer was {answer}.")
+        print(f"You got it! The answer is {answer}.")
 
 
 # Make function to set difficulty
@@ -30,22 +30,25 @@ def set_difficulty():
         return EASY_LEVEL_TURNS
     else:
         return HARD_LEVEL_TURNS
+    
+def game():
+  # Choosing a random number between 1 & 100
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+  answer = randint(1, 100)
+  print(f"The correct answer is {answer}.")
 
-# Choosing a random number between 1 & 100
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
-print(f"The correct answer is {answer}.")
+  turns = set_difficulty()
+  print(f"You have {turns} attempts remaining to guess the number.")
 
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number.")
-
-# Let user guess a number
-guess = int(input("Make a guess: "))
-
-check_answer(guess, answer)
+  # Repeat the guessing functionality if they get it wrong
+  guess = 0
+  while guess != answer:
+    # Let user guess a number
+    guess = int(input("Make a guess: "))
+    check_answer(guess, answer)
 
 
 # Track the number of turns remaining & reduce by 1 is they get it wrong
 
-# Repeat the guessing if they get it wrong
+game()
